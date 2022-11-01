0e5fa9368b8bacf135812d0ed94e78a3817ab495
hugomesquitah <hugomesquitacomp@gmail.com>
2022-11-01 01:44:41 -0300

    Add list key controller integration test with multiple fields

--- src/test/java/com/project/pixchallenge/web/KeyControllerIntegrationTest.java
+++ src/test/java/com/project/pixchallenge/web/KeyControllerIntegrationTest.java
@@ -117,12 +117,39 @@ public class KeyControllerIntegrationTest {
     }
 
     @Test
-    void when_listKey_expect_statusOk() throws Exception {
+    void when_listKeyWithIdFilter_expect_statusOk() throws Exception {
         var savedKey = keyRepository.save(KeyEntity.from(KeyBuilder.cpfCreated()));
 
         mockMvc.perform(get(BASE_URL)
                         .contentType(MediaType.APPLICATION_JSON)
                         .param("id", savedKey.getId().toString()))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.hasNext", equalTo(false)))
+                .andExpect(jsonPath("$.totalCount", equalTo(1)))
+                .andExpect(jsonPath("$.offset", equalTo(0)))
+                .andExpect(jsonPath("$.pageSize", equalTo(10)))
+                .andExpect(jsonPath("$.keys.[0].id", equalTo(savedKey.getId().toString())))
+                .andExpect(jsonPath("$.keys.[0].keyType", equalTo(savedKey.getType().name())))
+                .andExpect(jsonPath("$.keys.[0].keyValue", equalTo(savedKey.getValue())))
+                .andExpect(jsonPath("$.keys.[0].accountNumber", equalTo(savedKey.getAccountNumber())))
+                .andExpect(jsonPath("$.keys.[0].accountType", equalTo(savedKey.getAccountType().name())))
+                .andExpect(jsonPath("$.keys.[0].branchNumber", equalTo(savedKey.getBranchNumber())))
+                .andExpect(jsonPath("$.keys.[0].name", equalTo(savedKey.getName())))
+                .andExpect(jsonPath("$.keys.[0].lastName", equalTo(savedKey.getLastName())))
+                .andExpect(jsonPath("$.keys.[0].createdAt", notNullValue()));
+    }
+
+    @Test
+    void when_listKeyWithMultipleFilters_expect_statusOk() throws Exception {
+        var savedKey = keyRepository.save(KeyEntity.from(KeyBuilder.cpfCreated()));
+
+        mockMvc.perform(get(BASE_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .param("keyType", savedKey.getType().name())
+                        .param("accountNumber", savedKey.getAccountNumber().toString())
+                        .param("branchNumber", savedKey.getBranchNumber().toString())
+                        .param("name", savedKey.getName()))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.hasNext", equalTo(false)))
